a321ff17e98c22d604dc4194d5f88bd7a18b7386
claramarsango <claraxmarsango@gmail.com>
2023-02-03 02:40:54 +0100

    create TV show interface

---
+++ src/types/Series.ts
@@ -0,0 +1,12 @@
+interface Serie {
+  id: number;
+  name: string;
+  creator: string;
+  year: number;
+  poster: string;
+  watched: boolean;
+  score: number;
+  emmies: number;
+}
+
+export default Serie;
