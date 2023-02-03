c29e5a6a39075316653ca8fe22c4684eceeea3ed
claramarsango <claraxmarsango@gmail.com>
2023-02-03 02:45:48 +0100

    transpile previous changes

--- public/js/components/index.js
+++ public/js/components/index.js
@@ -1,12 +1,10 @@
 import Header from '../components/Header/Header.js';
 import Card from '../components/TvShowCard/TvShowCard.js';
 import { series } from '../series.js';
-
-const divContainer = document.querySelector<HTMLElement>('.container');
+const divContainer = document.querySelector('.container');
 const exampleHeader = new Header('My Series', divContainer, 'main-header');
 exampleHeader.render();
-
 series.forEach(tvShow => {
-  const tvShowCard = new Card(tvShow, divContainer, 'serie');
-  tvShowCard.render();
+    const tvShowCard = new Card(tvShow, divContainer, 'serie');
+    tvShowCard.render();
 });
